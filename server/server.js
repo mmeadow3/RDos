d473c1bf663971a98a50d0f311be0e797e38ac0a
Mike Meadows <mmeadow33@gmail.com>
2016-10-12 13:02:48 -0500

    logging system processes out

--- server/server.js
+++ server/server.js
@@ -7,36 +7,35 @@ var path = require("path"); /////needed for html hosting
 const React = require("react")
 const os = require('os');
 
-let CPUcore = os.cpus()
-// The process.arch property returns a String identifying the processor architecture that the Node.js process is currently running on.
-// x64 is 64 bit platform 
-let osArch = os.arch()
-
-console.log(CPUcore);
-console.log(osArch);
-
 
 const app = express()
 const server = Server(app)
 
+/////////////OS and CPU Logic///////////////////////
+function getInterval(){
+  os.cpus()
+  console.log(os.cpus()[0]);
+  os.freemem()  //////free mmemory
+  console.log(os.freemem(), "bytes of free memory");
+}
+setInterval(getInterval, 1000)
+// The process.arch property returns a String identifying the processor architecture that the Node.js process is currently running on.
+// x64 is 64 bit platform
+console.log(os.platform(), os.arch()); //////darwin x64
+/////////////
+console.log("Computer being used:", os.hostname());
+//////////Load Average//////////
+console.log("1,5,15 minutes load averages", os.loadavg());
+////////////////////////////////
+
+/////////////END OS and CPU Logic///////////////////////
+// console.log(os.networkInterfaces()); More Complicated, will return to this
+
+console.log();
+
+
 const PORT = process.env.PORT || 3000
-// const MONGODB_URL =  process.env.MONGODB_URL || 'mongodb://localhost:27017/RDos'
-//
-// // app.set('view engine', 'pug')
-//
-// app.use(express.static('public')) /////telling to use the public file for JS files
-//
-//
-// app.get('/',function(req,res){
-//   res.sendFile(path.join(__dirname + '/client/index.html'));
-//   //__dirname : It will resolve to your project folder.
-// });
-//
-//
-// mongoose.Promise = Promise
-// mongoose.connect(MONGODB_URL, () => {
-//   server.listen(PORT, () => console.log(`Server listening on port: ${PORT}`))
-// })
+
 app.listen(PORT, function () {
-  console.log('listening on port 3000!');
+  console.log(`listening on port ${PORT}`);
 });
