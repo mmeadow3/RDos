032b1f5e9d01a929b955a6d99e5d6d7bac94bdd1
Mike Meadows <mmeadow33@gmail.com>
2016-10-13 16:05:03 -0500

    added more console logs to make sure data is steady

--- server/server.js
+++ server/server.js
@@ -12,43 +12,46 @@ const app = express()
 const server = Server(app)
 
 /////////////OS and CPU Logic///////////////////////
+let cpus = os.cpus();
 function getInterval(){
-  os.cpus()
-  console.log(os.cpus()[0]);
+  for(let i = 0, len = cpus.length; i < len; i++){
+    console.log(`CPU ${i}`,os.cpus()[i].speed, os.cpus()[i].model);
+    console.log(
+     cpus[i].times.user / 1000, "number of seconds the CPU has spent in User mode",
+     cpus[i].times.nice / 1000, "number of seconds the CPU has spent in nice mode",
+     cpus[i].times.sys / 1000, "number of seconds the CPU has spent in sys mode",
+     cpus[i].times.idle / 1000, " number of seconds the CPU has spent in idle mode",
+     cpus[i].times.irq / 1000, "number of seconds the CPU has spent in irq mode"
+   )
+  }
   os.freemem()  //////free mmemory
   console.log(os.freemem()/((1048576)), "MB of free memory");
   console.log("system uptime", os.uptime(), "seconds");
 }
 console.log(os.totalmem()/((1073741824)), ":GB of total memory");
 setInterval(getInterval, 1000)
-// The process.arch property returns a String identifying the processor architecture that the Node.js process is currently running on.
-// x64 is 64 bit platform
 console.log("The Operating System is:", os.type(), os.arch()); //////darwin x64
-/////////////
 console.log("Computer being used:", os.hostname());
-//////////Load Average//////////
 console.log("1,5,15 minutes load averages", os.loadavg());
-////////////////////////////////
 console.log("User info", os.userInfo().username); /////could be useful
 
 /////////////END OS and CPU Logic///////////////////////
 // console.log(os.networkInterfaces()); More Complicated, will return to this
-///////////////logical processors 
-var cpus = os.cpus();
+///////////////logical processors
 
-for(var i = 0, len = cpus.length; i < len; i++) {
-    console.log("CPU %s:", i);
-    var cpu = cpus[i], total = 0;
-
-    for(var type in cpu.times) {
-        total += cpu.times[type];
-    }
-
-    for(type in cpu.times) {
-        console.log("\t", type, Math.round(100 * cpu.times[type] / total));
-    }
-}
-console.log();
+//
+// for(var i = 0, len = cpus.length; i < len; i++) {
+//     console.log("CPU %s:", i);
+//     var cpu = cpus[i], total = 0;
+//
+//     for(var type in cpu.times) {
+//         total += cpu.times[type];
+//     }
+//
+//     for(type in cpu.times) {
+//         console.log("\t", type, Math.round(100 * cpu.times[type] / total));
+//     }
+// }
 
 
 const PORT = process.env.PORT || 3000
