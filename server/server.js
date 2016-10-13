387fe501f512ca366131c14c4f6c9917e3951e99
Mike Meadows <mmeadow33@gmail.com>
2016-10-13 12:38:04 -0500

    adding more logs for desired data

--- server/server.js
+++ server/server.js
@@ -16,17 +16,20 @@ function getInterval(){
   os.cpus()
   console.log(os.cpus()[0]);
   os.freemem()  //////free mmemory
-  console.log(os.freemem(), "bytes of free memory");
+  console.log(os.freemem()/((1048576)), "MB of free memory");
+  console.log("system uptime", os.uptime(), "seconds");
 }
+console.log(os.totalmem()/((1073741824)), ":GB of total memory");
 setInterval(getInterval, 1000)
 // The process.arch property returns a String identifying the processor architecture that the Node.js process is currently running on.
 // x64 is 64 bit platform
-console.log(os.platform(), os.arch()); //////darwin x64
+console.log("The Operating System is:", os.type(), os.arch()); //////darwin x64
 /////////////
 console.log("Computer being used:", os.hostname());
 //////////Load Average//////////
 console.log("1,5,15 minutes load averages", os.loadavg());
 ////////////////////////////////
+console.log("User info", os.userInfo().username); /////could be useful
 
 /////////////END OS and CPU Logic///////////////////////
 // console.log(os.networkInterfaces()); More Complicated, will return to this
